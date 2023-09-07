86f964fa0dfd2b1190271dabe148a865ae4b294f
valentino-berdoy <berdoy.valentinoet36@gmail.com>
2023-09-07 15:46:05 -0300

    tunneando con durand

--- index.js
+++ index.js
@@ -2,14 +2,22 @@
 const botonAgregarAlCarrito = document.getElementById("agregarAlCarrito");
 const contadorCarrito = document.getElementById("contadorCarrito");
 
-// Inicializar el contador
-let cantidadEnCarrito = 0;
+// Recuperar la cantidad en el carrito desde localStorage
+let cantidadEnCarrito = parseInt(localStorage.getItem('cantidadEnCarrito')) || 0;
+
+// Inicializar el contador en la página
+contadorCarrito.textContent = cantidadEnCarrito;
 
 // Agregar un evento al botón "Añadir al carrito"
 botonAgregarAlCarrito.addEventListener("click", () => {
     // Incrementar la cantidad en el carrito
     cantidadEnCarrito++;
-    
     // Actualizar el contador en la página
     contadorCarrito.textContent = cantidadEnCarrito;
+
+    // Guardar la cantidad en el carrito en localStorage
+    localStorage.setItem('cantidadEnCarrito', cantidadEnCarrito);
 });
+
+
+console.log(cantidadEnCarrito);
